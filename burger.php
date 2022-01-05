0b8d5069a5d712731ccea53090fd967616c6608f
Giovanni <g.salmeri@mondodomani.org>
2022-01-05 15:50:25 +0100

    Prefixed mmenu files with burger-

--- burger.php
+++ burger.php
@@ -16,10 +16,10 @@ class YellowBurger {
         if ($name=="header") {
             $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
             $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}burger.css\" />\n";
-            $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}mmenu-light.css\" />\n";
-            $output .= "<script type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}mmenu-light.js\"></script>\n";
+            $output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}burger-mmenu-light.css\" />\n";
+            $output .= "<script type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}burger-mmenu-light.js\"></script>\n";
             $output .= "<script type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}burger.js\"></script>\n";
-            $output .= "<script type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}mmenu-light.polyfills.js\"></script>\n";
+            $output .= "<script type=\"text/javascript\" defer=\"defer\" src=\"{$extensionLocation}burger-mmenu-light.polyfills.js\"></script>\n";
         }
         return $output;
     }
